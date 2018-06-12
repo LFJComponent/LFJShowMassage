88c956f1e7359f7987991aecd9c2240735d89813
李方建 <lifangjian@baicm.com.cn>
2018-06-12 23:35:54 +0800

    sss

--- LFJShowManager/LFJShowManager.h
+++ LFJShowManager/LFJShowManager.h
@@ -15,6 +15,6 @@ FOUNDATION_EXPORT double LFJShowManagerVersionNumber;
 FOUNDATION_EXPORT const unsigned char LFJShowManagerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <LFJShowManager/PublicHeader.h>
-#import <LFJShowManager/STShowManager.h>
+#import "STShowManager.h"
 
 
